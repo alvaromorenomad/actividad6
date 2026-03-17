d0403a7e0fe52eb4c6a3d62ec73f955110756edd
alvaromorenomadariaga <alvaromorenomad@gmail.com>
2026-03-17 18:00:39 +0100

    creacion de todas las páginas básicas necesarias

---
+++ src/app/pages/actualiza-usuario/actualiza-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actualiza-usuario',
+  imports: [],
+  templateUrl: './actualiza-usuario.component.html',
+  styleUrl: './actualiza-usuario.component.css',
+})
+export class ActualizaUsuarioComponent {
+
+}
