68f47f2b116f845fc0e404622bb4e8f88a3269d1
alvaromorenomadariaga <alvaromorenomad@gmail.com>
2026-03-18 17:37:05 +0100

    config basic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: 'full', component: HomeComponent},
+    {path: "home", component: HomeComponent},
+    {path:"**", component: Error404Component}
+];
