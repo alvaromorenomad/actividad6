c1bd48ae1cfe385fec917ee0ce0304684e9ee665
alvaromorenomadariaga <alvaromorenomad@gmail.com>
2026-03-21 10:44:59 +0100

    componente formulario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { Error404Component } from './pages/error404/error404.component';
 import { VistaUsuarioComponent } from './pages/vista-usuario/vista-usuario.component';
+import { NuevoUsuarioComponent } from './pages/nuevo-usuario/nuevo-usuario.component';
 
 export const routes: Routes = [
     {path: "", pathMatch: 'full', component: HomeComponent},
     {path: "home", component: HomeComponent},
-    {path: "vistaUsuario", component: VistaUsuarioComponent},
+    {path: "vista-usuario", component: VistaUsuarioComponent},
+    {path: "nuevo-usuario", component:NuevoUsuarioComponent},
     {path:"**", component: Error404Component}
 ];
